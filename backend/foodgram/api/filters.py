306c618dd2c6d6454dcaf601a455fc0f46442ff6
veronika <veronika@v-pix.local>
2022-10-29 13:43:38 +0300

    Final version after review

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -43,7 +43,9 @@ class RecipeFilter(django_filters.FilterSet):
     # search_param = "name"
 
 class IngredientFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name', lookup_expr='istartswith')
+    name = django_filters.CharFilter(
+        field_name='name', lookup_expr='istartswith'
+    )
 
     class Meta:
         model = Ingredient
